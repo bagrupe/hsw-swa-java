47402a01b897b78e6aece75824439d171035400c
Bastian Grupe <bastian.grupe@icloud.com>
2021-12-28 22:27:54 +0100

    added mockmvc test

---
+++ 21-kennzeichen/src/test/java/de/hsw/kennzeichen/KennzeichenMockTests.java
@@ -0,0 +1,31 @@
+package de.hsw.kennzeichen;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class KennzeichenMockTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getKennzeichenAPI() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/kennzeichen")
+                .param("kennzeichen", "H-AX-1337")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.kennzeichen").exists());
+    }
+}
