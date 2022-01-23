ebfa3d1e3958ad8dcbd570d8311a03911e78742c
Bastian Grupe <bastian.grupe@icloud.com>
2022-01-23 15:12:31 +0100

    split clean into clean/preparesource

--- clean.ps1
+++ clean.ps1
@@ -45,21 +45,3 @@ Set-Location $PSScriptRoot/52-grpc-maven
 & mvn clean
 
 Set-Location $cwd
-
-$classpaths = Get-ChildItem -Path . -Include .classpath -Recurse -Hidden
-foreach($file in $classpaths) {
-    Remove-Item $file -Force
-}
-$projects = Get-ChildItem -Path . -Include .project -Recurse -Hidden
-foreach($file in $projects) {
-    Remove-Item $file -Force
-}
-$settings = Get-ChildItem -Path . -Include .settings -Recurse -Hidden
-foreach($file in $settings) {
-    Remove-Item $file -Recurse -Force
-}
-
-Set-Location $PSScriptRoot
-Remove-Item $PSScriptRoot/../hsw-swa-java.zip
-& zip -r $PSScriptRoot/../hsw-swa-java.zip . -x '*.git*' -x '*.DS_Store*'
-Set-Location $cwd
---
+++ preparesource.ps1
@@ -0,0 +1,19 @@
+. $PSScriptRoot/clean.ps1
+
+$classpaths = Get-ChildItem -Path . -Include .classpath -Recurse -Hidden
+foreach($file in $classpaths) {
+    Remove-Item $file -Force
+}
+$projects = Get-ChildItem -Path . -Include .project -Recurse -Hidden
+foreach($file in $projects) {
+    Remove-Item $file -Force
+}
+$settings = Get-ChildItem -Path . -Include .settings -Recurse -Hidden
+foreach($file in $settings) {
+    Remove-Item $file -Recurse -Force
+}
+
+Set-Location $PSScriptRoot
+Remove-Item $PSScriptRoot/../hsw-swa-java.zip
+& zip -r $PSScriptRoot/../hsw-swa-java.zip . -x '*.git*' -x '*.DS_Store*'
+Set-Location $cwd
